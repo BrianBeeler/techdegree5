b2a453f290c9ad6765094f657508bb32db5711bf
Brian Beeler <brianlbeeler@gmail.com>
2021-04-06 12:34:01 -0500

    Got modal dialog working.

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,25 @@ let createAndAppendTo = (htmlStr, parentTag) => {
 
 // Create the search template
 
-// createAndAppendTo((
-//     `<form action="#" method="get">
-//         <input type="search" id="search-input" class="search-input" placeholder="Search...">
-//         <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-//     </form>`
-// ), '.search-container');
-
 createAndAppendTo((
-    `<div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-        </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">first last</h3>
-            <p class="card-text">email</p>
-            <p class="card-text cap">city, state</p>
-        </div>
-    </div>`
-), '#gallery')
+    `<form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>`
+), '.search-container');
+
+// createAndAppendTo((
+//     `<div class="card">
+//         <div class="card-img-container">
+//             <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+//         </div>
+//         <div class="card-info-container">
+//             <h3 id="name" class="card-name cap">first last</h3>
+//             <p class="card-text">email</p>
+//             <p class="card-text cap">city, state</p>
+//         </div>
+//     </div>`
+// ), '#gallery')
 
 
 // createAndAppendTo((
@@ -74,14 +74,19 @@ function checkStatus(response) {
     }
 }
 
+modals = {};
 
 fetchData('https://randomuser.me/api/?results=12', 
     onSuccess = (data) => {
         console.log(data);
         data.results.forEach((user)=> {
-            html = createHTMLForUser(user)
-            createAndAppendTo(html, '.search-container')
+    
+            let html = createHTMLForUser(user)
+            createAndAppendTo(html, '#gallery')
+            modalHtml = createPopupHtmlForUser(user)
+            modals[user.login.uuid] = modalHtml
         })
+
     }, 
     onFailure = () => {
 
@@ -89,7 +94,7 @@ fetchData('https://randomuser.me/api/?results=12',
 
 let createHTMLForUser = (user) => {
     return (
-    `<div class="card">
+    `<div class="card" onclick="createPopup('${user.login.uuid}')">
         <div class="card-img-container">
             <img class="card-img" src="${user.picture.medium}" alt="profile picture">
         </div>
@@ -99,4 +104,38 @@ let createHTMLForUser = (user) => {
             <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
         </div>
     </div>`)
+}
+
+let createPopupHtmlForUser = (user) => {
+   return (
+   `<div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${user.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.city}</p>
+                <hr>
+                <p class="modal-text">${user.phone}</p>
+                <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city} ${user.location.postcode}</p>
+                <p class="modal-text">Birthday: ${formatBirthday(user.dob.date)}</p>
+            </div>
+        </div>
+
+        // IMPORTANT: Below is only for exceeds tasks 
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>
+    </div>`)
+}
+
+function formatBirthday(birthday) {
+    return birthday;
+}
+function createPopup(uuid) {
+    let userTemplate = modals[uuid];
+    console.log("userTemplate", userTemplate);
+    createAndAppendTo(userTemplate,'body');
 }
