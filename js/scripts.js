fa9ec07c1628e7d27d5f34530a42e8820b64fa62
Brian Beeler <brianlbeeler@gmail.com>
2021-04-06 14:37:35 -0500

    Dynamic search works

--- js/scripts.js
+++ js/scripts.js
@@ -11,52 +11,13 @@ let createAndAppendTo = (htmlStr, parentTag) => {
 createAndAppendTo((
     `<form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
-        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+        <input type="submit" onclick="searchUsers()" value="&#x1F50D;" id="search-submit" class="search-submit">
     </form>`
 ), '.search-container');
 
-// createAndAppendTo((
-//     `<div class="card">
-//         <div class="card-img-container">
-//             <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-//         </div>
-//         <div class="card-info-container">
-//             <h3 id="name" class="card-name cap">first last</h3>
-//             <p class="card-text">email</p>
-//             <p class="card-text cap">city, state</p>
-//         </div>
-//     </div>`
-// ), '#gallery')
 
 
-// createAndAppendTo((
-//     `<div class="modal-container">
-//     <div class="modal">
-//         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//         <div class="modal-info-container">
-//             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-//             <h3 id="name" class="modal-name cap">name</h3>
-//             <p class="modal-text">email</p>
-//             <p class="modal-text cap">city</p>
-//             <hr>
-//             <p class="modal-text">(555) 555-5555</p>
-//             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-//             <p class="modal-text">Birthday: 10/21/2015</p>
-//         </div>
-//     </div>
-
-//     // IMPORTANT: Below is only for exceeds tasks 
-//     <div class="modal-btn-container">
-//         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-//         <button type="button" id="modal-next" class="modal-next btn">Next</button>
-//     </div>
-//     </div>`
-// ), 'body')
-// Append the search template to the dom
-
-
-
-// Fetch function
+// Fetch User Info
 
 function fetchData(url, onSuccess, onFailure) {
     return fetch(url)
@@ -135,8 +96,6 @@ let createPopupHtmlForUser = (user) => {
 
 function formatBirthday(birthday) {
     let burthday = new Date(birthday)
-    
-
     return `${burthday.getMonth()}/${burthday.getDate()}/${burthday.getYear()}`;
 }
 function createPopup(uuid) {
@@ -179,5 +138,23 @@ function renderNextModal(uuid) {
 
     removeModel()
     createPopup(userData[index].login.uuid)
+
+}
+
+function searchUsers() {
+    let searchTerm = document.querySelector('#search-input').value;
+    filteredData = userData.filter( (user) => {
+        return user.name.first.includes(searchTerm) || user.name.last.includes(searchTerm)
+    })
+
+
+    document.querySelector('#gallery').innerHTML = '';
+
+
+    filteredData.forEach((user) => {
+        let html = createHTMLForUser(user)
+        createAndAppendTo(html, '#gallery')
+    })
+ 
 
 }
